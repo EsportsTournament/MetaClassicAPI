e41de62e9c732122f9db065881cf6b564cf31567
aditya-thakur <adityathakur532@gmail.com>
2022-06-04 01:28:39 +0530

    basic code runs

---
+++ main.js
@@ -0,0 +1,22 @@
+require('./db/db');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const properties = require('./config/properties');
+// const userRouter = require('./router/user.router');
+const app = express();
+
+app.listen(properties.port, () => {
+    console.log('Express is serving at port: ', properties.port);
+});
+
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// app.use('/user', userRouter);
+
+app.get('/', (req, res) => { res.json({ msg: 'Meta Classic Backend is up and live.' }) })
