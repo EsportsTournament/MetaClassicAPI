46490744ab3c415cd62abff7e08b82e92a475baf
aditya-thakur <adityathakur532@gmail.com>
2022-06-07 02:08:27 +0530

    added empty options in db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const properties = require('../config/properties');
 const mongoose = require('mongoose');
 console.log(properties.dburl);
-mongoose.connect(properties.dburl).then(
+mongoose.connect(properties.dburl, {}).then(
     () => { console.log('connected to db'); }
 ).catch(
     (err) => { console.log(err); }
